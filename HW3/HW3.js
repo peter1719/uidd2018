003919bc9a44482360c06c343fa05571ebabc926
peter1719 <peterpenno1@gmail>
2018-04-04 03:55:34 +0800

    finish?

--- HW3/HW3.js
+++ HW3/HW3.js
@@ -1,5 +1,5 @@
 var counter = 0, score = 0;
-var series = ["a","w","s","d","space"];
+var series = ["a","s","d","space","w"];
 var series_size = 5;
 var on_off = 1;
 
@@ -19,11 +19,9 @@ function order(o,limit)
         score+=5;
         random();        
         $(".flex-container").remove();
-        recolor();
-        add();
-       
+        document.getElementById("demo").innerHTML = "Score: " + score;
+        add();       
     }
-    
     counter = o;
 }
 
@@ -33,34 +31,35 @@ function Presskey(event,data,size,i)// w:119/87      s:115/83   a:97/65    d:100
     var x = event.which || event.keyCode;
 
     //document.getElementById("demo").innerHTML = "The Unicode value is: " + x;
-   
+   var id = "#d" + (i+1);
+   //document.getElementById("demo").innerHTML = id;
     if((x == 97 || x == 65 || x==37) && data[i]=="a")
     {
-        $("#a").css("background-image","url(pic/ay.png)");
+        $(id).css("background-image","url(pic/ay.png)");
         order(i,series_size-1);
     }
 
     else if((x == 119 || x == 87 || x==38 ) && data[i]=="w")
     {
-        $("#w").css("background-image","url(pic/wy.png)");
+        $(id).css("background-image","url(pic/wy.png)");
         order(i,series_size-1);
     }
 
     else if((x == 115 || x == 83 || x==40)  && data[i]=="s")
     {
-        $("#s").css("background-image","url(pic/sy.png)");
+        $(id).css("background-image","url(pic/sy.png)");
         order(i,series_size-1);
     }
 
     else if((x == 100 || x == 68 || x==39)  && data[i]=="d")
     {
-        $("#d").css("background-image","url(pic/dy.png)");
+        $(id).css("background-image","url(pic/dy.png)");
         order(i,series_size-1);
     }
 
     else if(x == 32  && data[i]=="space")
     {
-        $("#space").css("background-image","url(pic/spacey.png)");
+        $(id).css("background-image","url(pic/spacey.png)");
         order(i,series_size-1);
     } 
 }
@@ -97,46 +96,39 @@ function random()
 
 function add()
 {
-    $("body").append( "<div class=flex-container> </div>");
+    var txt = " ";
+    var id ;
+    $("body").append( "<div class=flex-container></div>"); 
     for (var i=0;i<series_size;i++)
     {
+        txt = "<div id=d" + (i+1) + "></div>";
+        id = "#d" + (i+1);
+        $(".flex-container").append( txt ); 
         switch(series[i])
         {
             case "w":
-            $(".flex-container").append( "<div id=w></div>"); 
-                      
+            $(id).css("background-image","url(pic/w.png)");          
             break;
             
             case "a":
-            $(".flex-container").append( "<div id=a></div>"); 
-            
+            $(id).css("background-image","url(pic/a.png)");
             break;
 
             case "s":
-            $(".flex-container").append( "<div id=s></div>"); 
-            
+            $(id).css("background-image","url(pic/s.png)");
             break;
 
             case "d":
-            $(".flex-container").append( "<div id=d></div>"); 
-            
+            $(id).css("background-image","url(pic/d.png)");
             break;
 
             case "space":
-            $(".flex-container").append( "<div id=space></div>"); 
-           
+            $(id).css("background-image","url(pic/space.png)");
             break;
-        }        
+        }   
     }
 }
-function recolor ()
-{
-    $("#space").css("background-image","url(pic/spacey.png)");
-    $("#d").css("background-image","url(pic/d.png)");
-    $("#s").css("background-image","url(pic/s.png)");
-    $("#a").css("background-image","url(pic/a.png)");
-    $("#w").css("background-image","url(pic/w.png)"); 
-}
+
 function fade(s)
 {
     $(s).fadeOut();
